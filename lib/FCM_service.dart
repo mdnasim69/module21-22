00e0fdd1cddb2a2a5f2219a8d90346a531996413
Nasim <mdnasimnasim2007@gmail.com>
2026-01-22 20:01:40 +0600

    1

---
+++ lib/FCM_service.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+class FCM_service {
+  FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+  Future<void> initalize()async{
+    await firebaseMessaging.requestPermission(
+      sound: true,
+      alert: true,
+    );
+    FirebaseMessaging.onMessage.listen((RemoteMessage msg){});
+    FirebaseMessaging.onMessageOpenedApp.listen(cloudMessage);
+    FirebaseMessaging.onBackgroundMessage(BackgroundCloudMessage);
+  }
+  void cloudMessage(RemoteMessage message){
+    print(message.data);
+    print(message.sentTime);
+    print(message.notification?.title);
+    print(message.notification?.body);
+    //TODO : do something
+  }
+}
+Future<void> BackgroundCloudMessage(RemoteMessage message) async{
+  //TODO : do something
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase/FCM_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FCM_service().initalize();
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
